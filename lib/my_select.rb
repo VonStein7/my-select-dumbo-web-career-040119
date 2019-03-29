f51bc6ac1e9ffa8e491d55011afbb22263691bd7
brenden williams <brendenw7@gmail.com>
2019-03-29 20:53:46 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,15 +1,15 @@
 def my_select(collection)
  # your code here!
   i = 0
-  item
+  item_arr = []
  
   while i < collection.length 
     if yield(collection[i]) == true 
-      item = yield(collection[i])
+      item_arr << yield(collection[i])
     end
     
     i += 1 
   end 
   
-  
+  item_arr
 end
