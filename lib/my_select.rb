6358233a93d7de9a9058fdce9c2fc1bb55fabbca
brenden williams <brendenw7@gmail.com>
2019-03-29 20:05:19 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
 def my_select(collection)
  # your code here!
+  i = 0
+  item
+ 
+  while i < collection.length 
+    if yield(collection[i]) == true 
+      item = yield(collection[i])
+    end
+    
+    i += 1 
+  end 
+  
+  
 end
